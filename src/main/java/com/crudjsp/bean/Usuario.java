d3c660a5ec73e688f8b38305c689a9142e20a8c0
Leandro Barbosa <boyleandro_208@hotmail.com>
2022-05-01 14:56:50 -0300

    first commit

---
+++ src/main/java/com/crudjsp/bean/Usuario.java
@@ -0,0 +1,11 @@
+package com.crudjsp.bean;
+
+public class Usuario {
+
+	private int id;
+	private String nome;
+	private String password;
+	private String email;
+	private String sexo;
+	private String pais;
+}
---
+++ src/main/java/com/crudjsp/dao/UsuarioDao.java
@@ -0,0 +1,5 @@
+package com.crudjsp.dao;
+
+public class UsuarioDao {
+
+}
